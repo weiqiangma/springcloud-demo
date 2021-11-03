c37b4707d8bed275b46bbfb1f82a3df1ecf08dff
17857026617 <mawkun@163.com>
2021-11-03 17:55:18 +0800

    cloud-config相关练习demo

--- eureka-gateway/src/main/java/com/mawkun/eurekagateway/EurekaGatewayApplication.java
+++ eureka-gateway/src/main/java/com/mawkun/eurekagateway/EurekaGatewayApplication.java
@@ -2,12 +2,16 @@ package com.mawkun.eurekagateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @EnableZuulProxy
+@EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class EurekaGatewayApplication {
 
